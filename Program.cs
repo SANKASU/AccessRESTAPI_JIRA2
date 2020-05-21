accfcba81134a79b5179542e3860493ed11a96e3
SANKASU <sureshbabu.sankarasubbu@hcahealthcare.com>
2020-05-21 17:59:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace AccessRESTAPI_JIRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseURL = ConfigurationManager.AppSettings["JIRABaseURL"];
+
+            //API to Invoke CreateMetadata
+            string CreateMetadataAPI = "issue/createmeta";
+        }
+    }
+}
